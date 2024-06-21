1b55e29d8c07b026a7450b1e68ae8bceee528efd
Yaroslav Zaprudskii <y.zaprudskii@gmail.com>
2024-06-21 23:31:04 +0300

    Add default props to Accordion

--- frontend/src/shared/ui/Accordion/index.tsx
+++ frontend/src/shared/ui/Accordion/index.tsx
@@ -1,15 +1,22 @@
 import { HTMLAttributes, Key, ReactNode, useState } from 'react';
 import './styles.css';
 
-interface Props<T> extends Omit<HTMLAttributes<HTMLDivElement>, 'content'> {
+interface OptionalProps<T> {
+  onDelete?: (item: T) => void;
+}
+
+interface Props<T> extends Omit<HTMLAttributes<HTMLDivElement>, 'content'>, OptionalProps<T> {
   sections: T[];
   rowKey: (item: T) => Key;
   header: (item: T) => ReactNode;
   content: (item: T) => ReactNode;
-  onDelete: (item: T) => void;
 }
 
-const Accordion = <T,>({ sections, rowKey, header, content, onDelete }: Props<T>) => {
+const defaultProps: OptionalProps<unknown> = {
+  onDelete: () => {},
+};
+
+const Accordion = <T,>({ sections, rowKey, header, content, onDelete, ...props }: Props<T>) => {
   const [openedKeys, setOpenedKeys] = useState<Key[]>([]);
 
   const toggleSection = (section: T) => {
@@ -21,8 +28,14 @@ const Accordion = <T,>({ sections, rowKey, header, content, onDelete }: Props<T>
   };
 
   const isOpened = (section: T) => openedKeys.includes(rowKey(section));
+
+  const handleDelete = (section: T) => {
+    if (onDelete) {
+      onDelete(section);
+    }
+  };
   return (
-    <div className='accordion'>
+    <div className='accordion' {...props}>
       {sections.map((section, index) => (
         <div key={rowKey(section)} className='accordion-item'>
           <div className={`accordion-header ${isOpened(section) ? 'border-b' : ''}`}>
@@ -35,7 +48,7 @@ const Accordion = <T,>({ sections, rowKey, header, content, onDelete }: Props<T>
             </div>
             <i
               className='fa fa-close text-error-600 cursor-pointer hover:bg-secondary-600 p-1 rounded'
-              onClick={() => onDelete(section)}
+              onClick={() => handleDelete(section)}
             />
           </div>
           <div className={`accordion-content  ${isOpened(section) ? 'active' : ''}`}>
@@ -46,5 +59,7 @@ const Accordion = <T,>({ sections, rowKey, header, content, onDelete }: Props<T>
     </div>
   );
 };
+
+Accordion.defaultProps = defaultProps;
 
 export default Accordion;
