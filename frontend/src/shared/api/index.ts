d469f86cad86150dfc85a2bb14411a9c70e9c7a1
Yaroslav Zaprudskii <y.zaprudskii@gmail.com>
2024-06-26 21:58:02 +0300

    Add base service

---
+++ frontend/src/shared/api/index.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+export default class BaseService<Data> {
+  private url: string;
+
+  constructor(url: string) {
+    this.url = url;
+  }
+
+  private getUrlWithId(id?: string) {
+    return id ? `${this.url}/${id}` : this.url;
+  }
+
+  get<Response = Data>(id?: string) {
+    return axios.get<Response, Response>(this.getUrlWithId(id));
+  }
+
+  getList<Response = Data[]>(id?: string) {
+    return axios.get<Response, Response>(this.getUrlWithId(id));
+  }
+
+  post<Request = Omit<Data, 'id'>, Response = Data>(data: Request, id?: string) {
+    return axios.post<Request, Response>(this.getUrlWithId(id), data);
+  }
+
+  put<Request = Data, Response = Data>(data: Request, id?: string) {
+    return axios.put<Request, Response>(this.getUrlWithId(id), data);
+  }
+
+  delete(id: string) {
+    return axios.delete(`${this.url}/${id}`);
+  }
+}
