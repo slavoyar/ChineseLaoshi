67270aae0910710d36e4b9e05dbce75ac9f84645
Yaroslav Zaprudskii <slavoyarmc@gmail.com>
2024-08-18 01:08:24 +0300

    Add prettier config to backend

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -1,46 +1,30 @@
 ﻿module.exports = {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-    parserOptions: {
-        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module', // Allows for the use of imports
-        project: './tsconfig.json', // Specify it only for TypeScript projects
-    },
-    extends: [
-        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-        'airbnb-typescript/base', // Uses the recommended rules from eslint-config-airbnb-typescript
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    project: './tsconfig.json', // Specify it only for TypeScript projects
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'airbnb-typescript/base', // Uses the recommended rules from eslint-config-airbnb-typescript
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'prettier', 'simple-import-sort', 'import'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'import/extensions': [
+      'warn',
+      'ignorePackages',
+      {
+        ts: 'never',
+      },
     ],
-    plugins: [
-        '@typescript-eslint',
-        'prettier',
-        'simple-import-sort',
-        'import',
-    ],
-    rules: {
-        "prettier/prettier": ["error", {
-            endOfLine: 'lf',
-            jsxSingleQuote: true,
-            printWidth: 100,
-            proseWrap: 'never',
-            quoteProps: 'as-needed',
-            semi: true,
-            singleQuote: true,
-            tabWidth: 2,
-            trailingComma: 'all',
-            useTabs: false,
-            vueIndentScriptAndStyle: false,
-        }],
-        'import/extensions': [
-            'warn',
-            'ignorePackages',
-            {
-                'ts': 'never',
-            }
-        ],
-        'simple-import-sort/imports': 'error', // Ensures that imports are sorted
-        'simple-import-sort/exports': 'error', // Ensures that exports are sorted
-        '@typescript-eslint/explicit-module-boundary-types': 'off', // Example rule customization
-        '@typescript-eslint/no-explicit-any': 'off', // Example rule customization
-        'import/prefer-default-export': 'off', // Example rule customization
-        'class-methods-use-this': 'off', // Example rule customization
-    },
+    'simple-import-sort/imports': 'error', // Ensures that imports are sorted
+    'simple-import-sort/exports': 'error', // Ensures that exports are sorted
+    '@typescript-eslint/explicit-module-boundary-types': 'off', // Example rule customization
+    '@typescript-eslint/no-explicit-any': 'off', // Example rule customization
+    'import/prefer-default-export': 'off', // Example rule customization
+    'class-methods-use-this': 'off', // Example rule customization
+  },
 };
