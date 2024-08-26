fbe7e53d0697b24a1ebfb99b62a5f3ae68d289f1
Yaroslav Zaprudskii <y.zaprudskii@gmail.com>
2024-08-26 17:57:52 +0300

    Refactor errors

---
+++ backend/src/configs/errors/errors.ts
@@ -0,0 +1,24 @@
+export const errors = {
+  loginError: {
+    message: 'Wrong username or password',
+    statusCode: 500,
+  },
+  registerError: {
+    message: 'Username or email is not unique',
+    statusCode: 500,
+  },
+  entityNotFoundError: {
+    message: 'Requested entity is not found',
+    statusCode: 404,
+  },
+  entityCreateError: {
+    message: 'Could not create an entity',
+    statusCode: 500,
+  },
+  entityUpdateError: {
+    message: 'Could not update an entity',
+    statusCode: 500,
+  },
+} as const;
+
+export type ErrorCode = keyof typeof errors;
