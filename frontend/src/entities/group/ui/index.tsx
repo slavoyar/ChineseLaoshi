f0c0bf4c45ef9936231a38473365585a183a885d
Yaroslav Zaprudskii <slavoyarmc@gmail.com>
2024-09-01 17:11:41 +0300

    Add write study page on main page

--- frontend/src/entities/group/ui/index.tsx
+++ frontend/src/entities/group/ui/index.tsx
@@ -1,9 +1,6 @@
 import { useGroupStore, Group } from '@entities/group';
 import { Accordion } from '@shared/ui';
 import { FC, ReactNode } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useCardStore } from '@entities/card';
-import { Route } from '@shared/types';
 import { GroupHeader } from './group-header';
 
 interface Props {
@@ -13,16 +10,9 @@ interface Props {
 
 export const GroupList: FC<Props> = ({ content, onGroupOpen }) => {
   const [groups, deleteGroup] = useGroupStore((state) => [state.groups, state.delete]);
-  const navigate = useNavigate();
-  const setIsStudy = useCardStore((state) => state.setIsStudy);
 
   const deleteHandler = async (item: Group) => {
     await deleteGroup(item.id);
-  };
-
-  const onStudyClick = () => {
-    setIsStudy(true);
-    navigate(`/${Route.StudyWrite}`);
   };
 
   return (
@@ -30,13 +20,6 @@ export const GroupList: FC<Props> = ({ content, onGroupOpen }) => {
       sections={groups}
       rowKey={(item) => item.id}
       header={(item) => <GroupHeader name={item.name} wordCount={item.wordCount} />}
-      actions={() => (
-        <i
-          className='fa fa-pencil-square hover:bg-secondary-600 rounded p-1 cursor-pointer'
-          title='Study'
-          onClick={() => onStudyClick()}
-        />
-      )}
       content={content}
       onOpen={onGroupOpen}
       onDelete={deleteHandler}
--- frontend/src/pages/main/index.tsx
+++ frontend/src/pages/main/index.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Route } from '@shared/types';
 import { Groups } from '@widgets/groups';
+import { StudyModes } from '@widgets/study-modes';
 
 export const Main = () => {
   const fetchGroups = useGroupStore((state) => state.fetch);
@@ -14,7 +15,8 @@ export const Main = () => {
     });
   }, []);
   return (
-    <div className='md:w-6/12 m-auto h-full'>
+    <div className='md:w-6/12 m-auto h-full flex flex-col gap-6'>
+      <StudyModes />
       <Groups />
     </div>
   );
