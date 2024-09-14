da135b85d0fcee2d009c169ed12bbf8a4bf52405
Yaroslav Zaprudskii <slavoyarmc@gmail.com>
2024-09-14 18:43:46 +0300

    Fix problems with main page layout

--- frontend/src/widgets/groups/index.tsx
+++ frontend/src/widgets/groups/index.tsx
@@ -2,8 +2,9 @@ import { CardList, useCardStore } from '@entities/card';
 import { Group, GroupList, useGroupStore } from '@entities/group';
 import { AddGroup } from '@features/add-group';
 import { AddWord } from '@features/add-word';
+import { HTMLAttributes } from 'react';
 
-export const Groups = () => {
+export const Groups = (props: HTMLAttributes<HTMLDivElement>) => {
   const [cardsPerGroup, fetchCards] = useCardStore((state) => [state.cardsPerGroup, state.fetch]);
   const decrementWordCount = useGroupStore((state) => state.decrementWordCount);
 
@@ -14,12 +15,15 @@ export const Groups = () => {
   };
 
   return (
-    <div className='md:p-10 p-5 bg-secondary-900 rounded-2xl flex flex-col md:gap-10 gap-5 h-fit max-h-full'>
+    <div
+      className='md:p-10 p-5 bg-secondary-900 rounded-2xl flex flex-col md:gap-10 gap-5 h-fit max-h-full'
+      {...props}
+    >
       <div className='flex items-center justify-between'>
         <div className='text-2xl text-white'>Folders</div>
         <AddGroup />
       </div>
-      <div className='overflow-auto h-full'>
+      <div className='overflow-auto h-full p-2'>
         <GroupList
           content={(item) => (
             <div>
