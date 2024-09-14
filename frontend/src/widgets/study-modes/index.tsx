73c12f12f1bb0267159b650aa66e874202ebe342
Yaroslav Zaprudskii <y.zaprudskii@gmail.com>
2024-09-14 20:27:07 +0300

    Develop (#42)
    * Fix merge problems

--- frontend/src/widgets/study-modes/index.tsx
+++ frontend/src/widgets/study-modes/index.tsx
@@ -1,10 +1,11 @@
 import { WriteStudy } from '@features/write-study';
+import { forwardRef } from 'react';
 
-export const StudyModes = () => (
-  <div className='flex flex-col gap-2'>
+export const StudyModes = forwardRef<HTMLDivElement>((_, ref) => (
+  <div ref={ref} className='flex flex-col gap-2'>
     <h1 className='text-white text-xl text-center'>Study modes</h1>
     <div className='w-full justify-center flex'>
       <WriteStudy />
     </div>
   </div>
-);
+));
