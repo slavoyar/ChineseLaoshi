77fd7a21b295ec1a109ceca6119ffb37f2e72132
Yaroslav Zaprudskii <y.zaprudskii@gmail.com>
2024-09-14 18:51:16 +0300

    Main page layout fixed (#39)
    * Add styles for scrolbar
    
    * Fix problems with main page layout
    
    * Fix CI

--- frontend/src/pages/main/index.tsx
+++ frontend/src/pages/main/index.tsx
@@ -1,11 +1,15 @@
 import { useGroupStore } from '@entities/group';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Route } from '@shared/types';
 import { Groups } from '@widgets/groups';
 import { StudyModes } from '@widgets/study-modes';
 
+const GAP = 24;
+const DEFAULT_OFFSET = 200;
+
 export const Main = () => {
+  const studyModesRef = useRef<HTMLDivElement>(null);
   const fetchGroups = useGroupStore((state) => state.fetch);
   const navigate = useNavigate();
 
@@ -14,10 +18,15 @@ export const Main = () => {
       navigate(Route.SignIn);
     });
   }, []);
+
   return (
     <div className='md:w-6/12 m-auto h-full flex flex-col gap-6'>
-      <StudyModes />
-      <Groups />
+      <StudyModes ref={studyModesRef} />
+      <Groups
+        style={{
+          maxHeight: `calc(100% - ${(studyModesRef.current?.offsetHeight ?? DEFAULT_OFFSET) + GAP}px`,
+        }}
+      />
     </div>
   );
 };
