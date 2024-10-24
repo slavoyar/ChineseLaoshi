7fb3bda7a483f9e8b38522c7a46800db20b3360a
Yaroslav Zaprudskii <slavoyarmc@gmail.com>
2024-10-24 18:15:12 +0300

    Rename feature for default practice lesson

--- frontend/src/features/start-write-practice/index.tsx
+++ frontend/src/features/start-write-practice/index.tsx
@@ -7,7 +7,7 @@ import { Autocomplete } from '@shared/ui/autocomplete';
 import { ChangeEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export const WriteStudy = () => {
+export const StartWritePractice = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const [setState, setSettings, settings] = useStateStore((state) => [
--- frontend/src/widgets/study-modes/index.tsx
+++ frontend/src/widgets/study-modes/index.tsx
@@ -1,11 +1,11 @@
-import { WriteStudy } from '@features/write-study';
+import { StartWritePractice } from '@features/start-write-practice';
 import { forwardRef } from 'react';
 
 export const StudyModes = forwardRef<HTMLDivElement>((_, ref) => (
   <div ref={ref} className='flex flex-col gap-2'>
     <h1 className='text-white text-xl text-center'>Study modes</h1>
     <div className='w-full justify-center flex'>
-      <WriteStudy />
+      <StartWritePractice />
     </div>
   </div>
 ));
