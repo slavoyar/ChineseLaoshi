1e49729569502d775151241796f737a3bea8b4ac
Yaroslav Zaprudskii <y.zaprudskii@gmail.com>
2025-04-02 20:19:51 +0300

    Remove try catch in repositories

--- backend/src/repositories/user.repository.ts
+++ backend/src/repositories/user.repository.ts
@@ -4,46 +4,30 @@ import type { CreateUserDto, UpdateUserDto } from '@shared/types';
 
 class UserRepository {
   async getById(id: string) {
-    try {
-      const user = await prisma.user.findFirst({ where: { id } });
-      if (!user) {
-        throw new CustomError('entityNotFoundError');
-      }
-      return user;
-    } catch {
+    const user = await prisma.user.findFirst({ where: { id } });
+    if (!user) {
       throw new CustomError('entityNotFoundError');
     }
+    return user;
   }
 
   async getByEmail(email: string) {
-    try {
-      const user = await prisma.user.findFirst({ where: { email } });
-      if (!user) {
-        throw new CustomError('entityNotFoundError');
-      }
-      return user;
-    } catch {
+    const user = await prisma.user.findFirst({ where: { email } });
+    if (!user) {
       throw new CustomError('entityNotFoundError');
     }
+    return user;
   }
 
   create(data: CreateUserDto) {
-    try {
-      return prisma.user.create({ data });
-    } catch {
-      throw new CustomError('entityCreateError');
-    }
+    return prisma.user.create({ data });
   }
 
   update(data: UpdateUserDto, userId: string) {
-    try {
-      return prisma.user.update({
-        where: { id: userId },
-        data,
-      });
-    } catch {
-      throw new CustomError('entityUpdateError');
-    }
+    return prisma.user.update({
+      where: { id: userId },
+      data,
+    });
   }
 }
 
--- backend/src/repositories/word.repository.ts
+++ backend/src/repositories/word.repository.ts
@@ -1,72 +1,47 @@
-import { CustomError } from '@configs/errors';
 import { prisma } from '@configs/prisma';
 import type { PrismaClient } from '@prisma/client/extension';
 import type { CreateWordDto, WordDto } from '@shared/types';
 
 class WordRepository {
   searchWord(query: string) {
-    try {
-      return prisma.word.findMany({
-        where: {
-          OR: [
-            { translation: { contains: query } },
-            { translation: { contains: query } },
-            { symbols: { contains: query } },
-          ],
-        },
-      });
-    } catch {
-      throw new CustomError('entityNotFoundError');
-    }
+    return prisma.word.findMany({
+      where: {
+        OR: [
+          { translation: { contains: query } },
+          { translation: { contains: query } },
+          { symbols: { contains: query } },
+        ],
+      },
+    });
   }
 
   createWord(data: CreateWordDto, client: PrismaClient = prisma) {
-    try {
-      return client.word.create({ data });
-    } catch {
-      throw new CustomError('entityCreateError');
-    }
+    return client.word.create({ data });
   }
 
   updateWord(data: Partial<WordDto>) {
-    try {
-      return prisma.word.update({ where: { id: data.id }, data });
-    } catch {
-      throw new CustomError('entityUpdateError');
-    }
+    return prisma.word.update({ where: { id: data.id }, data });
   }
 
   deleteWord(id: string, client: PrismaClient = prisma) {
-    try {
-      return client.word.delete({ where: { id } });
-    } catch {
-      throw new CustomError('entityDeleteError');
-    }
+    return client.word.delete({ where: { id } });
   }
 
   deleteWords(ids: string[]) {
-    try {
-      return prisma.word.deleteMany({ where: { id: { in: ids } } });
-    } catch {
-      throw new CustomError('entityDeleteError');
-    }
+    return prisma.word.deleteMany({ where: { id: { in: ids } } });
   }
 
   getWordsInOtherGroups(groupId: string, wordIds: string[]) {
-    try {
-      return prisma.card.groupBy({
-        by: ['wordId'],
-        where: {
-          wordId: { in: wordIds },
-          groupId: { not: groupId }, // Only count words in cards from other groups
-        },
-        _count: {
-          wordId: true,
-        },
-      });
-    } catch {
-      throw new CustomError('entityNotFoundError');
-    }
+    return prisma.card.groupBy({
+      by: ['wordId'],
+      where: {
+        wordId: { in: wordIds },
+        groupId: { not: groupId }, // Only count words in cards from other groups
+      },
+      _count: {
+        wordId: true,
+      },
+    });
   }
 }
 
